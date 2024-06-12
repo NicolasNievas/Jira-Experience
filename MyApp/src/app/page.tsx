3820323bb9e6f35e240dbe4586793a0d14c6d98b
Nicolas Nievas <nicoonievas7@gmail.com>
2024-06-12 18:04:44 -0300

    styless

--- MyApp/src/app/page.tsx
+++ MyApp/src/app/page.tsx
@@ -3,17 +3,17 @@ import Link from 'next/link';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 text-white">
+    <div className="w-full min-h-screen flex items-center justify-center bg-gradient-to-b from-black to-gray-800">
       <div className="flex flex-row justify-between w-full max-w-screen-lg">
         <div className="flex flex-col justify-center mr-8">
-          <h1 className="text-5xl font-extrabold mb-8 drop-shadow-lg">
+          <h1 className="text-5xl font-extrabold mb-8 drop-shadow-lg text-white">
             Welcome! Unify your tasks,<br />
             teammates and tools. <br />
           </h1>
           <div className="space-y-4"> 
             <Link href="/table">
               <button className="bg-white text-blue-500 hover:text-white hover:bg-blue-700 font-bold py-3 px-6 rounded-full shadow-lg transition-all duration-300">
-              Go to Work
+                Go to Work
               </button>
             </Link>
           </div>
@@ -23,7 +23,6 @@ const HomePage: React.FC = () => {
             <img src="https://blog.vantagecircle.com/content/images/2022/04/Improving-Teamwork-in-the-workplace.png" alt="Image" className="max-w-md rounded-lg" />
           </div>
         </div>
-
       </div>
     </div>
   );
